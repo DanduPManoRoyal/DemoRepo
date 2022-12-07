0f9e5b3f7821a72aff6f71ffa6e7ae487577a01c
D P <manohar.d-p@capgemini.com>
2022-12-07 11:59:07 +0530

    updated output value

--- server/src/main/java/com/example/Duplicatesstring.java
+++ server/src/main/java/com/example/Duplicatesstring.java
@@ -12,9 +12,9 @@ public class Duplicatesstring {
 		for(int i=0;i<str.length();i++)
 			set.add(str.charAt(i));
 			for(Character ch:set)
-				System.out.print(ch);
+				//System.out.print(ch);
 		
-		
+		System.out.println("Output value" +ch);
 	}
 	
 	
