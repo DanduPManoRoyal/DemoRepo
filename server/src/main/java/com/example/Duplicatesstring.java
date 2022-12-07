ca08ea11a6e6399213bd2032fb8ae715382c2b2f
D P <manohar.d-p@capgemini.com>
2022-12-07 11:53:31 +0530

    Added Stringclass

---
+++ server/src/main/java/com/example/Duplicatesstring.java
@@ -0,0 +1,27 @@
+package com.example;
+
+
+import java.util.LinkedHashSet;
+
+public class Duplicatesstring {
+
+	private static void remove(String str) {
+		
+		LinkedHashSet<Character> set=new LinkedHashSet<>();
+		
+		for(int i=0;i<str.length();i++)
+			set.add(str.charAt(i));
+			for(Character ch:set)
+				System.out.print(ch);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		String str="Manohar";
+		remove(str);
+	}
+
+
+}
